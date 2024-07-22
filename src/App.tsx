c567c44b14837b160759789d4446279f8a83faed
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-22 10:36:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Lista de participantes</h1>
+            <ol>
+                <li>Jesse Rodrigues</li>
+                {/* Coloca o nome aqui ó */}
+            </ol>
+        </>
+    );
+}
+
+export default App;
